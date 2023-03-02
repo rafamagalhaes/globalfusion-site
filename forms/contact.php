8e6a0017e9884a160d622c5e284fef4f3108f3ea
rafamagalhaes <rafa.magalhaes@gmail.com>
2023-03-02 10:21:24 -0300

    Fix contact mail

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'nelia.braga@globalfusionit.com.br';
+  $receiving_email_address = 'nelia.carvalho@globalfusion-it.com.br';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
